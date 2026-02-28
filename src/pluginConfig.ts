c9c39a57c484d3a069154a2edbc1aede6f80d117
chris <root@ChrisHP.localdomain>
2026-02-28 20:57:21 +0200

    picker bug

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-da',
-    version: '0.3.19',
+    version: '0.3.20',
     icon: '⛰',
     title: 'Multipicker',
     description:
